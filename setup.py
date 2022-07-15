de4a3527db95828f384e79a8ef60505876361282
Adrianna Pińska <adrianna.pinska@gmail.com>
2022-07-15 13:45:02 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="carta",
-    version="1.1.1",
+    version="1.1.2",
     author="Adrianna Pińska",
     author_email="adrianna.pinska@gmail.com",
     description="CARTA scripting wrapper written in Python",
