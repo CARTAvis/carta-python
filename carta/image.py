2be727a4c8cc580f5c68734f42c21cd89f5d9875
Adrianna Pińska <adrianna.pinska@gmail.com>
2022-09-06 17:15:00 +0200

    Bumped version. Added logic to aggregate HISTORY, COMMENT and blank entries into single entries.

--- carta/image.py
+++ carta/image.py
@@ -151,16 +151,61 @@ class Image:
 
         Returns
         -------
-        dict of string to string, integer, float or boolean
-            The header of the image, with field names as keys. T or F string values are automatically converted to booleans.
+        dict of string to string, integer, float, boolean, ``None`` or list of strings
+            The header of the image, with field names as keys. T or F string values are automatically converted to booleans. ``HISTORY``, ``COMMENT`` and blank keyword entries are aggregated into single entries with list values and with ``'HISTORY'``, ``'COMMENT'`` and ``''`` as keys, respectively. An entry in the history list which begins with ``'>'`` will be concatenated with the previous entry. Any other header entries with no values are given values of ``None``.
         """
         raw_header = self.get_value("frameInfo.fileInfoExtended.headerEntries")
-        header = {e["name"]: e.get("numericValue", e.get("value", None)) for e in raw_header}
-        for k, v in header.items():
-            if v == 'T':
-                header[k] = True
-            elif v == 'F':
-                header[k] = False
+
+        header = {}
+
+        history = []
+        comment = []
+        blank = []
+
+        def header_value(raw_entry):
+            try:
+                return raw_entry["numericValue"]
+            except KeyError:
+                try:
+                    value = raw_entry["value"]
+                    if value == 'T':
+                        return True
+                    if value == 'F':
+                        return False
+                    return value
+                except KeyError:
+                    return None
+
+        for raw_entry in raw_header:
+            name = raw_entry["name"]
+
+            if name.startswith("HISTORY "):
+                line = name[8:]
+                if line.startswith(">"):
+                    history[-1] = history[-1] + line[1:]
+                else:
+                    history.append(line)
+                continue
+
+            if name.startswith("COMMENT "):
+                comment.append(name[8:])
+                continue
+
+            if name.startswith(" " * 8):
+                blank.append(name[8:])
+                continue
+
+            header[name] = header_value(raw_entry)
+
+        if history:
+            header["HISTORY"] = history
+
+        if comment:
+            header["COMMENT"] = comment
+
+        if blank:
+            header[""] = blank
+
         return header
 
     @property
