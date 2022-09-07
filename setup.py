b6cc35d5bb3957ffcf9fe2552ce52d2f06e48518
Adrianna Pińska <adrianna.pinska@gmail.com>
2022-09-07 10:25:34 +0200

    Revert version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="carta",
-    version="1.1.6",
+    version="1.1.5",
     author="Adrianna Pińska",
     author_email="adrianna.pinska@gmail.com",
     description="CARTA scripting wrapper written in Python",
