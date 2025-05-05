9fa89de21cebe0d2ca63f2a65d347535368e35f4
Zhen-Kai Gao <zkgao@asiaa.sinica.edu.tw>
2025-05-05 12:07:19 +0800

    Add ColorBlending class with layer management and color blending functionality

---
+++ carta/colorblending.py
@@ -0,0 +1,414 @@
+from .constants import Colormap, ColormapSet
+from .image import Image
+from .util import BasePathMixin, CartaActionFailed, Macro, cached
+from .validation import (Boolean, Constant, Coordinate, InstanceOf, IterableOf,
+                         Number, validate)
+
+
+class Layer(BasePathMixin):
+    """This object represents a single layer in a color blending object.
+`
+    Parameters
+    ----------
+    colorblending : :obj:`carta.colorblending.ColorBlending`
+        The color blending object.
+    layer_id : int
+        The layer ID.
+
+    Attributes
+    ----------
+    colorblending : :obj:`carta.colorblending.ColorBlending`
+        The color blending object.
+    layer_id : int
+        The layer ID.
+    session : :obj:`carta.session.Session`
+        The session object associated with this layer.
+    """
+    def __init__(self, colorblending, layer_id):
+        self.colorblending = colorblending
+        self.layer_id = layer_id
+        self.session = colorblending.session
+
+        self._base_path = f"{self.colorblending._base_path}.frames[{layer_id}]"
+        self._frame = Macro("", self._base_path)
+
+    @classmethod
+    def from_list(cls, colorblending, layer_ids):
+        """
+        Create a list of Layer objects from a list of layer IDs.
+
+        Parameters
+        ----------
+        colorblending : :obj:`carta.colorblending.ColorBlending`
+            The color blending object.
+        layer_ids : list of int
+            The layer IDs.
+
+        Returns
+        -------
+        list of :obj:`carta.colorblending.Layer`
+                A list of new Layer objects.
+        """
+        return [cls(colorblending, layer_id) for layer_id in layer_ids]
+
+    def __repr__(self):
+        """A human-readable representation of this object."""
+        session_id = self.session.session_id
+        cb_id = self.colorblending.imageview_id
+        cb_name = self.colorblending.file_name
+        repr_content = [
+            f"{session_id}:{cb_id}:{cb_name}",
+            f"{self.layer_id}:{self.file_name}"
+        ]
+        return ":".join(repr_content)
+
+    @property
+    @cached
+    def file_name(self):
+        """The name of the image.
+
+        Returns
+        -------
+        string
+            The image name.
+        """
+        return self.get_value("frameInfo.fileInfo.name")
+
+    @property
+    @cached
+    def image_id(self):
+        """The ID of the image.
+
+        Returns
+        -------
+        int
+            The image ID.
+        """
+        return self.get_value("frameInfo.fileId")
+
+    @validate(Number(0, 1))
+    def set_alpha(self, alpha):
+        """Set the alpha value for the layer in the color blending.
+
+        Parameters
+        ----------
+        alpha : float
+            The alpha value.
+        """
+        self.colorblending.call_action("setAlpha", self.layer_id, alpha)
+
+    @validate(Constant(Colormap), Boolean())
+    def set_colormap(self, colormap, invert=False):
+        """Set the colormap for the layer in the color blending.
+
+        Parameters
+        ----------
+        colormap : :obj:`carta.constants.Colormap`
+            The colormap.
+        invert : bool
+            Whether the colormap should be inverted. This is false by default.
+        """
+        self.call_action(
+            "renderConfig.setColorMap", colormap)
+        self.call_action(
+            "renderConfig.setInverted", invert)
+
+
+class ColorBlending(BasePathMixin):
+    """This object represents a color blending image in a session.
+
+    Parameters
+    ----------
+    session : :obj:`carta.session.Session`
+        The session object associated with this color blending.
+    image_id : int
+        The image ID.
+
+    Attributes
+    ----------
+    session : :obj:`carta.session.Session`
+        The session object associated with this color blending.
+    image_id : int
+        The image ID.
+    """
+    def __init__(self, session, image_id):
+        self.session = session
+        self.image_id = image_id
+
+        path = "imageViewConfigStore.colorBlendingImages"
+        self._base_path = f"{path}[{self.image_id}]"
+        self._frame = Macro("", self._base_path)
+
+        self.base_frame = Image(
+            self.session, self.layer_list()[0].image_id)
+
+    @classmethod
+    def from_images(cls, session, images):
+        """Create a color blending object from a list of images.
+
+        Parameters
+        ----------
+        session : :obj:`carta.session.Session`
+            The session object.
+        images : list of :obj:`carta.image.Image`
+            The images to be blended.
+
+        Returns
+        -------
+        :obj:`carta.colorblending.ColorBlending`
+            A new color blending object.
+        """
+        # Set the first image as the spatial reference
+        session.call_action("setSpatialReference", images[0]._frame, False)
+        # Align the other images to the spatial reference
+        for image in images[1:]:
+            success = image.call_action(
+                "setSpatialReference", images[0]._frame)
+            if not success:
+                name = image.file_name
+                raise CartaActionFailed(
+                    f"Failed to set spatial reference for image {name}.")
+
+        command = "imageViewConfigStore.createColorBlending"
+        image_id = session.call_action(command, return_path="id")
+        return cls(session, image_id)
+
+    def __repr__(self):
+        """A human-readable representation of this color blending object."""
+        session_id = self.session.session_id
+        return f"{session_id}:{self.imageview_id}:{self.file_name}"
+
+    @property
+    @cached
+    def file_name(self):
+        """The name of the image.
+
+        Returns
+        -------
+        string
+            The image name.
+        """
+        return self.get_value("filename")
+
+    @property
+    @cached
+    def imageview_id(self):
+        """The ID of the image in imageView.
+
+        Returns
+        -------
+        integer
+            The image ID.
+        """
+        imageview_names = self.session.get_value(
+            "imageViewConfigStore.imageNames")
+        return imageview_names.index(self.file_name)
+
+    @property
+    def alpha(self):
+        """The alpha value list for the color blending layers.
+
+        Returns
+        -------
+        list of float
+            The alpha values.
+        """
+        return self.get_value("alpha")
+
+    def make_active(self):
+        """Make this the active image."""
+        self.session.call_action("setActiveImageByIndex", self.imageview_id)
+
+    def layer_list(self):
+        """
+        Returns a list of Layer objects, each representing a layer in
+        this color blending object.
+
+        Returns
+        -------
+        list of :obj:`carta.colorblending.Layer`
+            A list of Layer objects.
+        """
+        def count_layers():
+            idx = 0
+            while True:
+                try:
+                    self.get_value(f"frames[{idx}].frameInfo.fileId")
+                    idx += 1
+                except CartaActionFailed:
+                    break
+            return idx
+        return [Layer(self, i) for i in range(count_layers())]
+
+    def add_layer(self, image):
+        """Add a new layer to the color blending.
+
+        Parameters
+        ----------
+        image : :obj:`carta.image.Image`
+            The image to add.
+        """
+        self.call_action("addSelectedFrame", image._frame)
+
+    @validate(Number(1, None))
+    def delete_layer(self, layer_index):
+        """Delete a layer from the color blending.
+
+        Parameters
+        ----------
+        layer_index : int
+            The layer index. The base layer (layer_index = 0) cannot
+            be deleted.
+        """
+        self.call_action("deleteSelectedFrame", layer_index - 1)
+
+    @validate(InstanceOf(Image), Number(1, None))
+    def set_layer(self, image, layer_index):
+        """Set a layer at a specified index in the color blending.
+
+        Parameters
+        ----------
+        image : :obj:`carta.image.Image`
+            The image to set.
+        layer_index : int
+            The layer index. The base layer (layer_index = 0) cannot
+            be set.
+        """
+        self.call_action("setSelectedFrame", layer_index - 1, image._frame)
+
+    @validate(IterableOf(Number(1, None), min_size=2))
+    def reorder_layers(self, order_list):
+        """Reorder the layers in the color blending.
+
+        Parameters
+        ----------
+        order_list : list of int
+            The list of layer indices in the desired order. The list must not
+            contain the base layer (index = 0).
+        """
+        layers = self.layer_list()
+        image_ids = [layer.image_id for layer in layers]
+        # Delete all layers except the base layer
+        for _ in layers[1:]:
+            # Delete the first layer
+            # The previous second layer becomes the first layer
+            self.delete_layer(1)
+        for idx in order_list:
+            image = Image(self.session, image_ids[idx])
+            self.add_layer(image)
+
+    @validate(Coordinate(), Coordinate())
+    def set_center(self, x, y):
+        """Set the center position, in image or world coordinates.
+
+        World coordinates are interpreted according to the session's globally
+        set coordinate system and any custom number formats. These can be
+        changed using :obj:`carta.session.set_coordinate_system` and
+        :obj:`set_custom_number_format`.
+
+        Coordinates must either both be image coordinates or match the current
+        number formats. Numbers are interpreted as image coordinates, and
+        numeric strings with no units are interpreted as degrees.
+
+        Parameters
+        ----------
+        x : {0}
+            The X position.
+        y : {1}
+            The Y position.
+
+        Raises
+        ------
+        ValueError
+            If a mix of image and world coordinates is provided, if world
+            coordinates are provided and the image has no valid WCS
+            information, or if world coordinates do not match the session-wide
+            number formats.
+        """
+        self.base_frame.set_center(x, y)
+
+    @validate(Number(), Boolean())
+    def set_zoom_level(self, zoom, absolute=True):
+        """Set the zoom level.
+
+        TODO: explain this more rigorously.
+
+        Parameters
+        ----------
+        zoom : {0}
+            The zoom level.
+        absolute : {1}
+            Whether the zoom level should be treated as absolute. By default
+            it is adjusted by a scaling factor.
+        """
+        self.base_frame.set_zoom_level(zoom, absolute)
+
+    @validate(Constant(ColormapSet))
+    def set_colormap_set(self, colormap_set):
+        """Set the colormap set for the color blending.
+
+        Parameters
+        ----------
+        colormap_set : :obj:`carta.constants.ColormapSet`
+            The colormap set.
+        """
+        self.call_action("applyColormapSet", colormap_set)
+        for layer in self.layer_list():
+            layer.call_action("renderConfig.setInverted", False)
+
+    @validate(IterableOf(Number(0, 1)))
+    def set_alpha(self, alpha_list):
+        """Set the alpha value for the color blending layers.
+
+        Parameters
+        ----------
+        alpha_list : list of float
+            The alpha values.
+        """
+        layer_list = self.layer_list()
+        for alpha, layer in zip(alpha_list, layer_list):
+            layer.set_alpha(alpha)
+
+    @validate(Boolean())
+    def set_raster_visible(self, state):
+        """Set the raster component visibility.
+
+        Parameters
+        ----------
+        state : bool
+            The desired visibility state.
+        """
+        is_visible = self.get_value("rasterVisible")
+        if is_visible != state:
+            self.call_action("toggleRasterVisible")
+
+    @validate(Boolean())
+    def set_contour_visible(self, state):
+        """Set the contour component visibility.
+
+        Parameters
+        ----------
+        state : bool
+            The desired visibility state.
+        """
+        is_visible = self.get_value("contourVisible")
+        if is_visible != state:
+            self.call_action("toggleContourVisible")
+
+    @validate(Boolean())
+    def set_vectoroverlay_visible(self, state):
+        """Set the vector overlay visibility.
+
+        Parameters
+        ----------
+        state : bool
+            The desired visibility state.
+        """
+        is_visible = self.get_value("vectorOverlayVisible")
+        if is_visible != state:
+            self.call_action("toggleVectorOverlayVisible")
+
+    def close(self):
+        """Close this color blending object."""
+        self.session.call_action(
+            "imageViewConfigStore.removeColorBlending", self._frame)
